bc1fdb1656bae4c7b1d163f842aa68f0824fcbf7
ashkan <ashkan.rdn@gmail.com>
2025-03-26 20:43:44 -0400

    add next logger

--- lib/store.ts
+++ lib/store.ts
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from './features/filters/filterSlice';
 import mapReducer from './features/map/mapSlice';
+import { logger } from 'redux-logger'
 
+const defaultMiddlewareConfig = {
+  serializableCheck: {
+    ignoredPaths: ['pdf.data'],
+  }
+};
 export const store = configureStore({
   reducer: {
     filters: filterReducer,
     map: mapReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(defaultMiddlewareConfig).concat(logger)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
